fde48cb09c339376af3b9af3f43ac3db9a923e02
Heather Kirk <hskirk@gmail.com>
2019-01-22 17:26:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
  if katz_deli.length == 0 
  puts "The line is currently empty."
 else 
-  katz_deli.each do |number, name|
+  katz_deli.each_with_index do |number, name|
     puts "The line is currently #{number.to_i}, #{name}."
   end  
 end
