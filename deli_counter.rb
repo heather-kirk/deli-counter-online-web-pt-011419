d6034d0019a5e469f5b2a3fa500854db614d24be
Heather Kirk <hskirk@gmail.com>
2019-01-22 17:21:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+ if katz_deli.length == 0 
+ puts "The line is currently empty."
+else 
+  katz_deli.each do |number, name|
+    puts "The line is currently #{number.to_i}, #{name}."
+  end  
+end
+end
